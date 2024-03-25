2efe05d61fb703ba747f3e14bd2e8c9f712ec9cb
Sherry <e24089020@gs.ncku.edu.tw>
2024-03-25 17:16:24 +0800

    complete log in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const errorHandler = require("../utils/error");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -20,11 +22,29 @@ const signup = async (req, res, next) => {
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-  } catch (err) {
     //check if the email exists
-    const ValidUser = await userModel.findOne({ email });
+    const validUser = await userModel.findOne({ email });
 
-    if (!ValidUser) return next();
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    // check the password is correct or not
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong password"));
+
+    //create a jwt token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    //set the cookie
+    const { password: password_2, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      })
+      .json({ status: 200, data: rest });
+  } catch (err) {
+    next(err);
   }
 };
 module.exports = { signup, login };
