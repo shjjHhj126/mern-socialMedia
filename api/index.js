b05f94816e6e449668d5746da2ba371d45f38212
Sherry <e24089020@gs.ncku.edu.tw>
2024-04-02 19:35:40 +0800

    fix the cors origin

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://your-frontend-render-url.onrender.com",
+    origin: "https://mern-socialmedia-dxas.onrender.com",
     credentials: true,
   })
 );
